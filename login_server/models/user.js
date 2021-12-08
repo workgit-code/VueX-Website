aa7dc7a921dce401fb29558a14939c32b1a7564a
i397284 <i397284@fhict.nl>
2021-12-08 13:17:56 +0100

    added to the user their XP and stars

--- login_server/models/user.js
+++ login_server/models/user.js
@@ -5,9 +5,10 @@ const UserSchema = new Schema({
     username: String,
     email: String,
     password: String,
+    experience: Int32,
+    stars: Int32,
 })
 
 const User = mongoose.model('User', UserSchema)
 
 module.exports = User;
-
