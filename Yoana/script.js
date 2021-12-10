cf3d4bfaf59936f6da103d4c57d181021efe0270
Churkina,Yoana Y.K <82328697+Yoana01@users.noreply.github.com>
2021-12-10 11:34:33 +0100

    experience-new version without back-end and working front-end

--- Yoana/script.js
+++ Yoana/script.js
@@ -5,12 +5,3 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-
-  $(function() {
-    let username = sessionStorage.getItem("username")
-    let level = sessionStorage.getItem("level")
-    let stars = sessionStorage.getItem("stars")
-    document.querySelector("#user").innerHTML = username
-    document.querySelector("#level").innerHTML += level
-    document.querySelector("#stars").innerHTML = stars + '<img src="./images/Star.png" alt="stars">'
-  })
