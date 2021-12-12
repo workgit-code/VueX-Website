af3b82a2fd111753b50e2a558d8931e981e63927
Valkov,Momchil M.I <m.valkov@student.fontys.nl>
2021-12-12 18:17:02 +0100

    Merging js code in one file

--- Footer, nav and pages/script.js
+++ Footer, nav and pages/script.js
@@ -1,7 +1,32 @@
+//Sidenav
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+//Pofile image change
+const image_input = document.querySelector("#image_input");
+var uploaded_image;
+
+image_input.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    uploaded_image = reader.result;
+    document.querySelector(
+      "#profilePic"
+    ).style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutationRecord) {
+    console.log("style changed!");
+  });
+});
+
+var target = document.getElementById("profilePic");
+observer.observe(target, { attributes: true, attributeFilter: ["style"] });
