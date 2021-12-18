97f27cb9c61fb2b9668adc4f8872a3b81f384f31
Sesil <s.tasim@student.fontys.nl>
2021-12-18 19:32:04 +0100

    frontend changes - notification, friendslist, reviews

--- public/js/friends.js
+++ public/js/friends.js
@@ -6,10 +6,20 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-function showFriendSearch(){
+function showFriendSearch() {
   let overlayDiv = document.querySelector(".overlay");
-  if (overlayDiv.style.opacity === '1')
-    overlayDiv.style.opacity = 0;
-  else
-    overlayDiv.style.opacity = 1;
+  if (overlayDiv.style.opacity === "1") overlayDiv.style.opacity = 0;
+  else overlayDiv.style.opacity = 1;
 }
+
+
+//add the friend when the user clicks the icon and toggle
+const targetFriend = document.getElementById("AddYoana");
+const buttonIcon = document.getElementById("addFriend");
+buttonIcon.onclick = function () {
+  if (targetFriend.style.display !== "none") {
+    targetFriend.style.display = "none";
+  } else {
+    targetFriend.style.display = "block";
+  }
+};
